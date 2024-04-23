204181c34cedee78a79febd782a1b574d5644c40
ArpitaKaushal <arpitakaushal538@gmail.com>
2024-04-23 13:19:12 +0530

    Saving User Details

---
+++ task19/index.js
@@ -0,0 +1,57 @@
+function handleFormSubmit(event) {
+    event.preventDefault();
+    const userDetails = {
+      username: event.target.username.value,
+      email: event.target.email.value,
+      phone: event.target.phone.value,
+    };
+    axios
+      .post(
+        "https://crudcrud.com/api/dfcf7088bde448839030572170d9d2e7/appointmentData",
+        userDetails
+      )
+      .then((response) => displayUserOnScreen(response.data))
+      .catch((error) => console.log(error));
+  
+    // Clearing the input fields
+    document.getElementById("username").value = "username";
+    document.getElementById("email").value = "email";
+    document.getElementById("phone").value = "phone";
+  }
+  
+  function displayUserOnScreen(userDetails) {
+    const userItem = document.createElement("li");
+    userItem.appendChild(
+      document.createTextNode(
+        `${userDetails.username} - ${userDetails.email} - ${userDetails.phone}`
+      )
+    );
+  
+    const deleteBtn = document.createElement("button");
+    deleteBtn.appendChild(document.createTextNode("Delete"));
+    userItem.appendChild(deleteBtn);
+  
+    const editBtn = document.createElement("button");
+    editBtn.appendChild(document.createTextNode("Edit"));
+    userItem.appendChild(editBtn);
+  
+    const userList = document.querySelector("ul");
+    userList.appendChild(userItem);
+  
+    deleteBtn.addEventListener("click", function (event) {
+      userList.removeChild(event.target.parentElement);
+      localStorage.removeItem(userDetails.email);
+    });
+  
+    editBtn.addEventListener("click", function (event) {
+      userList.removeChild(event.target.parentElement);
+      localStorage.removeItem(userDetails.email);
+      document.getElementById("username").value = userDetails.username;
+      document.getElementById("email").value = userDetails.email;
+      document.getElementById("phone").value = userDetails.phone;
+    });
+  }
+  
+  // Do not touch code below
+  // module.exports = handleFormSubmit;
+  
